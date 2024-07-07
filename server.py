ca457dd311755a430a3e835375614403b17a6656
unknown <dusanthansivarajah@hotmail.com>
2024-07-07 23:27:02 +0300

    modified server.py and added main.py

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
+import yaml
 class Server:
 
-    def __init__(self, name):
-        self.name = name
+    def load_config(file_path='config.yaml'):
+        with open(file_path, 'r') as file:
+            config = yaml.safe_load(file)
+        return config
 
 
-server = Server("")
+    config = load_config()
 
+
+    if config['run_localhost']:
+        print("Error: Running on localhost is not allowed.")
